f4e778cc936361a01409a46a821e41f9c15194d0
phenix1229 <paul.garay@codeimmersives.com>
2020-04-17 14:21:51 -0400

    created user and ticket models

---
+++ routes/tickets/models/Ticket.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const today = () =>{
+    return `${new Date().getMonth()+1}/${new Date().getDate()}/${new Date().getFullYear()}`;
+};
+
+const TicketSchema = new mongoose.Schema({
+    openedBy:{type:String},
+    openDate:{default:today},
+    client:{type:String},
+    issue:{type:string},
+    resolution:{type:String},
+    closedBy:{type:String},
+    closeDate:{type:String}
+});
